7246bd0a607ef170a464731935fdf63b73dcad75
RyogaF22 <fairusyawalryoga@gmail.com>
2025-08-10 23:44:00 +0800

    READ TEMPERATURE, HUMIDITY, LIGHT AND MOTION

--- IOT/smartluxnighlight/smartluxnighlight.ino
+++ IOT/smartluxnighlight/smartluxnighlight.ino
@@ -1,5 +1,19 @@
+#include <DHT.h>
+#define DHTType DHT11
+
+const int lightSensorPin  = 25;
+const int motionSensorPin = 26;
+const int DHTPin          = 4;
+
+DHT dht(DHTPin, DHTType);
+
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(lightSensorPin,  INPUT);
+  pinMode(motionSensorPin, INPUT);
+  dht.begin();
 
 }
 
