aaaf0fe4e305368ea5139ef196c318bee127898a
Nor Hisamudin <37838526+hisam00@users.noreply.github.com>
2025-08-15 17:55:11 +0800

    RGB LED INTEGRATION

--- IOT/smartluxnighlight/smartluxnighlight.ino
+++ IOT/smartluxnighlight/smartluxnighlight.ino
@@ -4,6 +4,9 @@
 const int lightSensorPin  = 25;
 const int motionSensorPin = 26;
 const int DHTPin          = 4;
+const int redPin          = 14;
+const int greenPin        = 12;
+const int bluePin         = 13;
 
 DHT dht(DHTPin, DHTType);
 
@@ -13,7 +16,14 @@ void setup() {
   Serial.begin(115200);
   pinMode(lightSensorPin,  INPUT);
   pinMode(motionSensorPin, INPUT);
+  pinMode(redPin,          OUTPUT);
+  pinMode(greenPin,        OUTPUT);
+  pinMode(bluePin,         OUTPUT);
   dht.begin();
+
+  analogWrite(redPin,   0);
+  analogWrite(greenPin, 0);
+  analogWrite(bluePin,  0);
 
 }
 
