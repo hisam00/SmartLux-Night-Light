c50084b097353891c0b950c1ee877ceb4d3ac7ba
azimkhan05 <145893609+azimkhan05@users.noreply.github.com>
2025-08-27 23:12:44 +0800

    IMPROVE THE FIREBASE CONFIGURATION
    help with fix the user page problem where it replace the current admin account with newly added user account.

--- WEBSITE/public/firebase-config.js
+++ WEBSITE/public/firebase-config.js
@@ -1,16 +1,16 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
-import { sendEmailVerification } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBu4mqM1PeTHW4AKoP3e8Nhcx4jiHtwueU",
   authDomain: "smartlux-night-light.firebaseapp.com",
   databaseURL: "https://smartlux-night-light-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "smartlux-night-light",
-  storageBucket: "smartlux-night-light.firebasestorage.app",
+  storageBucket: "smartlux-night-light.appspot.com",
   messagingSenderId: "558742892847",
   appId: "1:558742892847:web:30b85deeca4102cb7f71da",
   measurementId: "G-MD4BV2GWYF"
@@ -20,5 +20,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
+const storage = getStorage(app);
 
-export { auth, db, createUserWithEmailAndPassword, doc, setDoc, sendEmailVerification };
+export { auth, db, createUserWithEmailAndPassword, doc, setDoc, sendEmailVerification, storage, ref, uploadBytes, getDownloadURL };
